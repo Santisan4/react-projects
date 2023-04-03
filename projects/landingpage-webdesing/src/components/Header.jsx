6435e413d8a29b4938c77344c3d2ffdf8f8001b2
Santiago Sanchez <sant97@hotmail.com.ar>
2023-04-03 14:40:58 -0300

    fixed margin

--- projects/landingpage-webdesing/src/components/Header.jsx
+++ projects/landingpage-webdesing/src/components/Header.jsx
@@ -5,7 +5,7 @@ export function Header () {
   const [showMenu, setShowMenu] = useState(false)
   return (
     <header className='flex items-center justify-between xl:justift-start py-4 px-8 w-full h-[10vh] z-50 md:px-20 xl:flex xl:py-0 xl:grid-cols-[(1fr, 2fr)]'>
-      <div className='xl:text-end xl:w-1/6 text-center -mt-4'>
+      <div className='text-center -mt-4 xl:text-end xl:w-1/6 '>
         <a href='#' className='text-[24px] font-bold relative pl-1 bg-white text-terceary'>
           Power<span className='text-primary text-5xl'>.</span>{' '}
           <RiCheckboxBlankCircleFill className='absolute -left-3 -bottom-3 text-primary rounded-tr-full' />
@@ -25,7 +25,7 @@ export function Header () {
           Products
         </a>
       </nav>
-      <button onClick={() => setShowMenu(!showMenu)} className='xl:hidden text-3xl p-2'>
+      <button onClick={() => setShowMenu(!showMenu)} className=' text-3xl -m-2 xl:hidden'>
         {showMenu ? <RiCloseLine /> : <RiMenuLine />}
 
       </button>
